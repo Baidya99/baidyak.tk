2548356c85acb771121cee9d93a7fb7e747e2786
Baidya99 <kundubaidya99@gmail.com>
2016-12-31 00:13:39 +0530

    Added shadow to header

---
+++ index.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  var head = function(){
+    scrollTop = $(window).scrollTop();
+    if(scrollTop>0){
+      $('#H').addClass('shadow');
+    }else{
+      $('#H').removeClass('shadow');
+    }
+  };
+
+  $(window).scroll(function() {
+	  head();
+  });
+});
