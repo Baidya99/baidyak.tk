cad6be7e4e3ad042d74dfd3b38c1031be4993b5e
Baidya99 <kundubaidya99@gmail.com>
2017-01-02 17:21:24 +0530

    Added a style

--- index.js
+++ index.js
@@ -3,8 +3,16 @@ $(document).ready(function(){
     scrollTop = $(window).scrollTop();
     if(scrollTop>0){
       $('#H').addClass('shadow');
+      $('#H').addClass('whiteb');
+      $('#H').removeClass('wtext');
+      $('#icon').removeClass('big');
+      $('#icon').addClass('small');
     }else{
       $('#H').removeClass('shadow');
+      $('#H').removeClass('whiteb');
+      $('#H').addClass('wtext');
+      $('#icon').removeClass('small');
+      $('#icon').addClass('big');
     }
   };
   $(window).scroll(function() {
